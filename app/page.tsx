76f5584ca1d78bc4f09ed8884d99b8a4324a59ac
comunidadevencedores-lgtm <comunidadevencedores@gmail.com>
2026-07-22 17:26:14 -0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
-import Gallery from "@/components/Gallery";
 import FAQ from "@/components/FAQ";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
@@ -16,7 +15,6 @@ export default function Home() {
         <Hero />
         <About />
         <Services />
-        <Gallery />
         <FAQ />
         <CTA />
       </main>
